88baf328bf2df5f0988d2d61b5a66ce0d977ce95
SumatiGupta <42660697+SumatiGupta@users.noreply.github.com>
2022-11-09 17:01:56 -0500

    Description comment added

--- raytracer/csfml_render.h
+++ raytracer/csfml_render.h
@@ -1,8 +1,11 @@
 //
-//  csfml_main.h
-//  raytracer
+//  File name: csfml_render.h
 //
-//  Created by Krzysztof Gabis on 13.04.2013.
+//  Authors: Created by Krzysztof Gabis on 01.04.2013 
+//           Modified by CSI 5640 Team (Max Theisen, Sumati Gupta, Prajakta Jadhav, Priya Kharde, Oluwakemi Ibrahim) on 11.09.2022
+//
+//  File contains: Declarations for file "csfml_render.c"
+//
 //  Copyright (c) 2013 Krzysztof Gabis. All rights reserved.
 //
 
